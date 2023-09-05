51fd0a1c1c92935a59e0a8bed4babd590356569e
Kosuke Takahashi <rhcp.kdog@gmail.com>
2023-09-06 00:06:09 +0900

    Add plop package.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+// TODO
+// コンポーネントでスタイリングせずに layout.tsxでスタイリングをすべきなのか
+// layout.tsxの役割はどこからどこまでなのか
+// いつも通りのアプリ作成をしてみる
+// react hook formを導入して日記アプリを作成する
+// figma or miroで簡易的なワイヤーフレームを作成する
+
 export default function Page() {
   return <h1>Hello, Next.js!</h1>
 }
