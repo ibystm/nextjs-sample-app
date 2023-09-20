1ea8a91a2cbf7671a2416b4ef9183771d3379267
Kosuke Takahashi <rhcp.kdog@gmail.com>
2023-09-20 23:56:41 +0900

    Impl Template.

--- plopfile.js
+++ plopfile.js
@@ -1,19 +1,32 @@
-export default function (plop) {
+module.exports = function (
+  /** @type {import('plop').NodePlopAPI} */
+  plop
+) {
   // controller generator
   plop.setGenerator('controller', {
     description: 'application controller logic',
     prompts: [
       {
         type: 'input',
+        name: 'path',
+        message: 'どこにStoryファイルを置きますか？(例: src/components/)',
+      },
+      {
+        type: 'input',
         name: 'name',
-        message: 'controller name please',
+        message: 'コンポーネント名を入力してください。',
       },
     ],
     actions: [
       {
         type: 'add',
-        path: 'src/{{name}}.tsx',
-        // templateFile: 'plop-templates/controller.hbs',
+        path: '{{path}}/{{pascalCase name}}/index.tsx',
+        templateFile: 'plop-templates/component.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: '{{path}}/{{pascalCase name}}/index.stories.tsx',
+        templateFile: 'plop-templates/stories.tsx.hbs',
       },
     ],
   })
